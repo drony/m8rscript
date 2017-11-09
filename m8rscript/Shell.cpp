0f44e0458f87b5fa7a3bffc899cce9b7a84ecc0f
Chris Marrin <chris@marrin.com>
2017-11-08 17:17:11 -0800

    Fixed crash when shell gets deleted before end task callback fires

--- m8rscript/Shell.cpp
+++ m8rscript/Shell.cpp
@@ -333,8 +333,9 @@ bool Shell::executeCommand(const std::vector<m8r::String>& array)
         showMessage(MessageType::Info, ROMSTR("erased all files\n"));
     } else if (array[0] == "run") {
         load((array.size() < 2) ? nullptr : array[1].c_str(), _debug);
-        run([this]{
-            _application->system()->printf(ROMSTR("\n***** Program Finished *****\n\n"));
+        Application* app = _application;
+        run([app]{
+            app->system()->printf(ROMSTR("\n***** Program Finished *****\n\n"));
         });
         _state = State::NeedPrompt;
         showMessage(MessageType::Info, ROMSTR("Program started...\n"));
